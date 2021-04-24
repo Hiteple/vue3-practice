4933fa2ca7ba78172d1df04f69a3fee4399973ff
hiteple <hiteplegold@gmail.com>
2021-04-24 01:31:09 -0300

    Changes to prepared code and setting up the service to obtain events

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>A site for events to make the world a better place</h1>
   </div>
 </template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>Events for Good</h1>
+   <div class="events">
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+   </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue';
+import EventsService from '../services/EventsService'
+
+export default {
+   name: 'EventList',
+   components: {
+      EventCard
+   },
+   data() {
+      return {
+         events: []
+      }
+   },
+   created() {
+      // using myjson server in Github
+      EventsService.getEvents()
+         .then(response => this.events = response.data)
+         .catch(error => console.log(error));
+   }
+}
+</script>
+
+<style scoped>
+   .events {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+   }
+</style>
